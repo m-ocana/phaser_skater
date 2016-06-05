e11a07bd30e18d7e3391a089c302517a4a9a4936
Manuel <manuelocanyacanyada@gmail.com>
2016-06-05 23:41:14 +0100

    massive refactor

--- src/states/Main.js
+++ src/states/Main.js
@@ -5,100 +5,50 @@ class Main extends Phaser.State {
 	create() {
 
 		//Enable Arcade Physics
-        this.game.physics.startSystem(Phaser.Physics.ARCADE);
-        this.game.plugins.add(Phaser.Plugin.ArcadeSlopes);
+        this.game.physics.startSystem(Phaser.Physics.P2JS);
+        //this.game.physics.setBoundsToWorld();
+        this.game.physics.p2.gravity.y = 1600;
 
         //Set the games background colour
         this.game.stage.backgroundColor = '#f76943';
 
         //Adding the Tilemap
 		this.map = this.game.add.tilemap('tilemap');
-		this.map.addTilesetImage('ninja-tiles', 'tiles');
+		this.map.addTilesetImage('ninja-tiles64', 'tiles');
 
-        //Config DayCycle Plugin
-        /*this.dayCycle = new DayCycle(this.game, 50000);
- 
-        let bgBitMap = this.game.add.bitmapData(this.game.width, this.game.height);
- 		
-        bgBitMap.ctx.rect(0, 0, this.game.width, this.game.height);
-        bgBitMap.ctx.fillStyle = '#b2ddc8';
-        bgBitMap.ctx.fill();
- 
-        this.backgroundSprite = this.game.add.sprite(0, 0, bgBitMap);
- 		
-        this.sunSprite = this.game.add.sprite(50, -250, 'sun');
-        this.moonSprite = this.game.add.sprite(this.game.width - (this.game.width / 4), this.game.height + 500, 'moon');
- 		*/
- 		//Parallax Images
-		/*this.buildingsBack = this.game.add.tileSprite(0, 0, 
-			this.map.widthInPixels, 
-			this.game.cache.getImage('buildings-back').height, 
-			'buildings-back'
-		);*/
-
+		// Adding background sprites
 		this.buildingsMiddle = this.game.add.tileSprite(0, 0,
 			this.map.widthInPixels, 
 			this.game.cache.getImage('buildings-middle').height, 
 			'buildings-middle'
 		);
-
 		this.buildingsFront = this.game.add.tileSprite(0, 0,
 			this.map.widthInPixels, 
 			this.game.cache.getImage('buildings-front').height, 
 			'buildings-front'
 		);
- 
-        /*let backgroundSprites = [
-            {sprite: this.backgroundSprite, from: 0x1f2a27, to: 0xB2DDC8},
-            {sprite: this.mountainsBack, from: 0x2f403b, to: 0x96CCBB},
-            {sprite: this.mountainsMid1, from: 0x283632, to: 0x8BBCAC},
-            {sprite: this.mountainsMid2, from: 0x202b28, to: 0x82AD9D}
-        ];
- 
-        this.dayCycle.initShading(backgroundSprites);
-        this.dayCycle.initSun(this.sunSprite);
-        this.dayCycle.initMoon(this.moonSprite);*/
 
         //Adding Ground & collisions
-		this.ground = this.map.createLayer('collision');
+		this.ground = this.map.createLayer('groundLayer');
+		this.ramps = this.map.createLayer('tiledRampsLayer');
 		this.ground.resizeWorld();
 		 
-		this.game.slopes.convertTilemapLayer(this.ground, {
-		    2:  'FULL',
-		    3:  'HALF_BOTTOM_LEFT',
-		    4:  'HALF_BOTTOM_RIGHT',
-		    6:  'HALF_TOP_LEFT',
-		    5:  'HALF_TOP_RIGHT',
-		    15: 'QUARTER_BOTTOM_LEFT_LOW',
-		    16: 'QUARTER_BOTTOM_RIGHT_LOW',
-		    17: 'QUARTER_TOP_RIGHT_LOW',
-		    18: 'QUARTER_TOP_LEFT_LOW',
-		    19: 'QUARTER_BOTTOM_LEFT_HIGH',
-		    20: 'QUARTER_BOTTOM_RIGHT_HIGH',
-		    21: 'QUARTER_TOP_RIGHT_HIGH',
-		    22: 'QUARTER_TOP_LEFT_HIGH',
-		    23: 'QUARTER_LEFT_BOTTOM_HIGH',
-		    24: 'QUARTER_RIGHT_BOTTOM_HIGH',
-		    25: 'QUARTER_RIGHT_TOP_LOW',
-		    26: 'QUARTER_LEFT_TOP_LOW',
-		    27: 'QUARTER_LEFT_BOTTOM_LOW',
-		    28: 'QUARTER_RIGHT_BOTTOM_LOW',
-		    29: 'QUARTER_RIGHT_TOP_HIGH',
-		    30: 'QUARTER_LEFT_TOP_HIGH',
-		    31: 'HALF_BOTTOM',
-		    32: 'HALF_RIGHT',
-		    33: 'HALF_TOP',
-		    34: 'HALF_LEFT'
-		});
-		 
-		this.map.setCollisionBetween(2, 34, true, 'collision');
-
+		this.map.setCollisionBetween(2,34,true,'groundLayer');
+		
+		this.game.physics.p2.convertTilemap(this.map, this.ground);
+		this.game.physics.p2.convertCollisionObjects(this.map,"ramps");
+		
 		//Add Player
-		this.player = this.game.add.sprite(64, 768, 'player');
-		this.game.physics.arcade.enable(this.player);
-		this.player.body.gravity.y = 500;
+		this.player = this.game.add.sprite(64, 1024, 'player');
+
+		this.game.physics.p2.enable(this.player);
+		this.player.anchor.setTo(0.5,0.5); // set the anchor to the exact middle of the player (good for flipping the image on the same place)
+    
+    	this.player.body.setCircle(64,0,0);
+    	this.player.body.fixedRotation=true;
+		//this.player.body.gravity.y = 200;
 		 
-		this.game.slopes.enable(this.player);
+		//this.game.slopes.enable(this.player);
 		this.game.camera.follow(this.player);
 
 		this.controls = this.input.keyboard.addKeys({
@@ -111,38 +61,69 @@ class Main extends Phaser.State {
 
 	update() {
 
-		this.game.physics.arcade.collide(this.player, this.ground);
-
-		//Parallax parameters
-	    //this.mountainsBack.tilePosition.x -= 0.05;
-	    //this.mountainsMid1.tilePosition.x -= 0.3;
-	    //this.mountainsMid2.tilePosition.x -= 0.75;
-
 		if (this.controls.left.isDown){
-			if(this.player.body.touching.down){
-				this.player.body.velocity.x -= 10;
+			if(this._touchingDown(this.player)){
+				// flips image to the left
+				this.player.scale.x = -1;
+				this.player.body.velocity.x -= 20;
 			} else {
 				this.player.body.velocity.x -= 2;
+				this.player.angle-=1;
 			}
 		}
 
 		if (this.controls.right.isDown){
-			if(this.player.body.touching.down){
-				this.player.body.velocity.x += 10;
+			if(this._touchingDown(this.player)){
+				this.player.scale.x = 1;
+				this.player.body.velocity.x += 20;
 			} else {
 				this.player.body.velocity.x += 2;
+				this.player.angle+=1;
 			}
 		}
 
-		if (this.controls.up.isDown && this.player.body.touching.down) {
+		if (this.controls.up.isDown && this._touchingDown(this.player)) {
 			this.player.body.velocity.y -= 400;
 		}
 
-		if (this.player.body.bottom >= this.world.bounds.bottom) {
+		if (this.player.bottom >= this.world.bounds.bottom) {
 		    this.state.start("Main");
 		}
 
+    this._angle(this.player);
+    //if (!this._touchingDown(this.player) ){this.player.angle=0;}
+
 	}
+
+	/*
+	this function uses the p2 collision calculations that are done on every step to find out if the player collides with something on the bottom - it returns true if a collision is happening
+	*/
+	_touchingDown(someone) {
+	    var yAxis = p2.vec2.fromValues(0, 1);
+	    var result = false;
+	    for (var i = 0; i < this.game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+	        var c = this.game.physics.p2.world.narrowphase.contactEquations[i];  // cycles through all the contactEquations until it finds our "someone"
+	        if (c.bodyA === someone.body.data || c.bodyB === someone.body.data)        {
+	            var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
+	            if (c.bodyA === someone.body.data) d *= -1;
+	            if (d > 0.5) result = true;
+	        }
+	    } return result;
+	}
+
+	_angle(someone) {
+    var yAxis = p2.vec2.fromValues(0, 1);
+    var result = 1;
+    for (var i = 0; i < this.game.physics.p2.world.narrowphase.contactEquations.length; i++) {
+        var c = this.game.physics.p2.world.narrowphase.contactEquations[i];  // cycles through all the contactEquations until it finds our "someone"
+        if (c.bodyA === someone.body.data || c.bodyB === someone.body.data)        {
+            var d = p2.vec2.dot(c.normalA, yAxis); // Normal dot Y-axis
+           // if (c.bodyA === someone.body.data) d *= -1;
+            someone.angle = c.normalA[0]*100;
+            //console.log(c.normalA[0])
+        }
+    } return result;
+}
 
 }
 
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -9,11 +9,11 @@ class Preload extends Phaser.State {
     	//this.game.load.image('sun', 'assets/sun.png');
         //this.game.load.image('moon', 'assets/moon.png');
 
-    	this.game.load.tilemap('tilemap', 'assets/tiles.json', null, Phaser.Tilemap.TILED_JSON);
+    	this.game.load.tilemap('tilemap', 'assets/test-tile-map.json', null, Phaser.Tilemap.TILED_JSON);
     	this.game.load.spritesheet('tiles', 'assets/ninja-tiles64.png', 64, 64);
 
     	//this.game.load.image('player', 'assets/player.png');
-        this.game.load.image('player', 'assets/skater.png');
+        this.game.load.image('player', 'assets/skater-white.png');
 	}
 
 	create() {
